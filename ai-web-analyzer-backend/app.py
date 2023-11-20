8d755eee98349e507f6a5f293c1c8411d700c2b1
Darnell <darnellgarcia26@gmail.com>
2023-11-20 09:42:20 -0400

    Addition of local MongoBD database and connection, in future, add local envs to keep data safe and add business logic for web scraping all pages and OpenAI API integration for summarizing

--- ai-web-analyzer-backend/app.py
+++ ai-web-analyzer-backend/app.py
@@ -1,9 +1,31 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_pymongo import PyMongo
 from flask_cors import CORS
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/AI-Web-Analyzer"
+mongo = PyMongo(app)
 CORS(app, origins="http://localhost:5173")
 
 @app.route('/')
 def hello():
     return {'message': 'Hello from Flask!'}
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    try:
+        # Ensures the collection exists in the MongoDB database
+        if 'websites' in mongo.db.list_collection_names():
+            # Retrieve data from  collection
+            data = list(mongo.db.websites.find())
+            return jsonify({'data': data})
+        else:
+            return jsonify({'data': []})  # Return an empty list if the collection doesn't exist
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+@app.route('/api/add_data', methods=['POST'])
+def add_data():
+    new_data = request.get_json()
+    mongo.db.websites.insert_one(new_data)
+    return jsonify({'message': 'Data added successfully'})
--- ai-web-analyzer/src/App.tsx
+++ ai-web-analyzer/src/App.tsx
@@ -3,6 +3,8 @@ import axios from 'axios';
 
 function App() {
   const [message, setMessage] = useState('');
+  const [websiteURL, setWebsiteURL] = useState('');
+  const [data, setData] = useState<string>('');
 
   useEffect(() => {
     axios.get('http://localhost:5000/')
@@ -10,11 +12,51 @@ function App() {
       .catch(error => console.error('Error fetching data:', error));
   }, []);
 
+  useEffect(() => {
+    const apiUrl = 'http://localhost:5000/api/data';
+    axios.get(apiUrl)
+      .then(response => setData(JSON.stringify(response.data)))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const apiUrl = 'http://localhost:5000/api/add_data';
+
+      // Make a POST request to add data
+      await axios.post(apiUrl, { websiteURL });
+
+      setWebsiteURL('');
+
+      // Fetch updated data after adding new data
+      const updatedData = await axios.get('http://localhost:5000/api/data');
+      setData(JSON.stringify(updatedData.data));
+    } catch (error) {
+      console.error('Error submitting form:', error);
+    }
+  };
+
   return (
-    <div>
-      <p>{message}</p>
-      <p>IDK IF this is working</p>
-    </div>
+    <>
+      <div>
+        <h1>AI Web Analyzer</h1>
+        <h2>Website URL</h2>
+        <form onSubmit={handleFormSubmit}> {/*TODO: use Post later on when pushing to DB*/}
+          <input 
+            id='websiteURL' 
+            type="text" 
+            placeholder="Enter your desired website here" 
+            value={websiteURL} 
+            onChange={event => setWebsiteURL(event.target.value)}
+          />
+          <input type="submit" value="Submit"></input>
+        </form>
+        <p>{message}</p>
+        <p>{data}</p>
+      </div>
+    </>
   );
 }
 
