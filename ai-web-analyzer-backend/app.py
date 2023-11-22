5a523fbb8865b78a41bf4c27670e8486974327b1
Darnell <darnellgarcia26@gmail.com>
2023-11-21 23:25:43 -0400

    Added functionality for OpenAI integrstion for URL summaries and added css changes.

--- ai-web-analyzer-backend/app.py
+++ ai-web-analyzer-backend/app.py
@@ -5,15 +5,32 @@ from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 import bleach
 import requests
+from openai import OpenAI
+from dotenv import load_dotenv
+from os.path import join, dirname
+import os
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/AI-Web-Analyzer"
 mongo = PyMongo(app)
 CORS(app, origins="http://localhost:5173")
 
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+
+client = OpenAI(
+  api_key=os.environ['OPENAI_API_KEY']
+)
+
+initialPrompt = "Write a summary on the below text, this text was directly parsed from a URL in a specific website for context. "
+
+
+
+
 @app.route('/')
 def hello():
-    return {'message': 'Hello from Flask!'}
+    return {'message': 'Hi!'}
 
 #TODO: implement function to retrieve data from backend, when searching for a specific URL
 
@@ -35,6 +52,9 @@ def add_data():
     new_data = request.get_json()
     mongo.db.websites.insert_one(new_data)
     return jsonify({'message': 'Data added successfully'})
+
+
+#TODO: clear the database when a new website is added.
 
 @app.route('/api/add_website', methods=['POST'])
 def add_website():
@@ -79,12 +99,32 @@ def add_website():
                     queue.append(full_url)
                     print(full_url)
         
-        return jsonify({'message': 'Website URLs added successfully, choose a page to summarize or load another website above!'})
+        return jsonify({'message': 'Website URLs added successfully, choose a page to summarize or load another website below!'})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
                 
-# @app.route('/api/website_summary', methods=['GET'])
-# def website_summary(site):
-#     #first check if website is in list
-    
-#     # if it is then perform summary.
+@app.route('/api/website_summary', methods=['POST'])
+def website_summary():
+    try:
+        data = request.get_json()
+        target_url = data.get('target_url')
+
+        # Retrieve text content from MongoDB based on the target URL
+        website_data = mongo.db.websites.find_one({'url': target_url})
+
+        if website_data:
+            text_content = website_data.get('text_content', '')
+
+            # Use OpenAI to summarize the text
+            summary = client.completions.create(
+                model="text-davinci-002",
+                prompt=initialPrompt + text_content,
+                max_tokens=800  # Adjust max_tokens as needed
+            )['choices'][0]['text']
+
+            return jsonify({'summary': summary})
+        else:
+            return jsonify({'error': 'Website data not found'}), 404
+    except Exception as e:
+        print(f"Error in website_summary: {e}")
+        return jsonify({'error': str(e)}), 500
--- ai-web-analyzer/src/App.tsx
+++ ai-web-analyzer/src/App.tsx
@@ -2,8 +2,12 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function App() {
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState('Database not connected!');
+  const [summaryMessage, setSummaryMessage] = useState('Nothing to summarize...');
   const [websiteURL, setWebsiteURL] = useState('');
+  const [summaryURL, setSummaryURL] = useState('');
+
+  const [isLoaded, setLoaded] = useState(false);
   // const [data, setData] = useState<string>('');
 
   useEffect(() => {
@@ -32,6 +36,8 @@ function App() {
       .then(response => setMessage(response.data.message));
 
       setWebsiteURL('');
+      setLoaded(true)
+      console.log(isLoaded)
 
       // Fetch updated data after adding new data
       // const updatedData = await axios.get('http://localhost:5000/api/data');
@@ -44,37 +50,70 @@ function App() {
 
   const handleSummarySubmit = async (event) => {
     event.preventDefault();
-    alert("Summarized page info :)")
+
+    console.log(isLoaded)
+
+    if(isLoaded == false)
+    {
+      setSummaryURL('');
+      alert("There is no website loaded, please load a website first...")
+      return;
+    }
     // TODO: add check to ensure that a website is loaded before this function can run, a simple boolean value can work.
+
+    try {
+      const apiUrl = 'http://localhost:5000/api/website_summary';
+
+      // Make a POST request to add data
+      await axios.post(apiUrl, { target_url: summaryURL })
+      .then(response => setSummaryMessage(response.data.summary));
+
+      setSummaryURL('');
+
+      // Fetch updated data after adding new data
+      // const updatedData = await axios.get('http://localhost:5000/api/data');
+      // setData(JSON.stringify(updatedData.data));
+    } 
+    catch (error) {
+      console.error('Error submitting form:', error);
+    }
   };
 
   return (
     <>
       <div>
-        <h1>AI Web Analyzer</h1>
-        <h2>Website URL</h2>
-        <p>{message}</p>
-        <form onSubmit={handleWebsiteSubmit}> {/*TODO: use Post later on when pushing to DB*/}
-          <input 
-            id='websiteURL' 
-            type="text" 
-            placeholder="Enter your desired website here" 
-            value={websiteURL} 
-            onChange={event => setWebsiteURL(event.target.value)}
-          />
-          <input type="submit" value="Submit"></input>
-        </form>
-        <h2>Summary URL</h2>
-        <form onSubmit={handleSummarySubmit}> {/*TODO: use Post later on when pushing to DB*/}
-          <input 
-            id='websiteURL' 
-            type="text" 
-            placeholder="Enter your desired website here" 
-            value={websiteURL} 
-            onChange={event => setWebsiteURL(event.target.value)}
-          />
-          <input type="submit" value="Submit"></input>
-        </form>
+        <header id='title-header'>
+          <h1>AI Web Analyzer</h1>
+        </header>
+        <main>
+          <h2>Website URL</h2>
+          <p>{message}</p>
+          <form onSubmit={handleWebsiteSubmit}> {/*TODO: use Post later on when pushing to DB*/}
+            <input 
+              id='websiteURL' 
+              type="text" 
+              placeholder="Enter your desired website here" 
+              value={websiteURL} 
+              onChange={event => setWebsiteURL(event.target.value)}
+            />
+            <input type="submit" value="Submit"></input>
+          </form>
+          <h2>Summary URL</h2>
+          <form onSubmit={handleSummarySubmit}> {/*TODO: use Post later on when pushing to DB*/}
+            <input 
+              id='summaryURL' 
+              type="text" 
+              placeholder="Enter the URL you want to summarize" 
+              value={summaryURL} 
+              onChange={event => setSummaryURL(event.target.value)}
+            />
+            <input type="submit" value="Submit"></input>
+          </form>
+          <h2>Summary Output:</h2>
+          <section id='summary-section'>
+            <p>{summaryMessage}</p>
+          </section>
+        </main>
       </div>
     </>
   );
